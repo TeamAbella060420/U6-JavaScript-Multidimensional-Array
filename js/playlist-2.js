4e513d8dff1cb801a691d6c11ff12d0492954430
TeamAbella060420 <jabella@rscm.com.sa>
2022-04-08 11:50:03 +0300

    Update playlist-2.js

--- js/playlist-2.js
+++ js/playlist-2.js
@@ -1,16 +1,19 @@
 const playlist = [
-  'So What',
-  'Respect',
-  'What a Wonderful World',
-  'At Last',
-  'Three Little Birds',
-  'The Way You Look Tonight'
+  ['So What', 'Miles Davis', '9:04'],
+  ['Respect', 'Aretha Franklin', '2:45'],
+  ['What a wonderful world', 'Louis Armstrong', '2:21'],
+  ['At Last', 'Ella Fitzgerald', '4:18'],
+  ['Three Little Birds', 'Bob Marley and the Wailers', '3:01'],
+  ['The way you look tonight', 'Frank Sinatra', '3:21']
 ];
+
+// const myArtist = `${playlist[0][1]}, ${playlist[1][1]}, ${playlist[5][1]}`;
+// console.log(myArtist);
 
 function createListItems( arr ) {
   let items = '';
   for ( let i = 0; i < arr.length; i++ ) {
-    items += `<li>${ arr[i] }</li>`;
+    items += `<li>${ arr[i][0]}, by ${ arr[i][1]} - ${ arr[i][2]}</li>`;
   }
   return items;
 }
@@ -19,4 +22,4 @@ document.querySelector('main').innerHTML = `
   <ol>
     ${createListItems(playlist)}
   </ol>
-`;
+`;  
